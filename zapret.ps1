2b271635bd7b47d6f3da90bc09c79188a0c74b32
Seva <68725282+sevcator@users.noreply.github.com>
2025-02-22 22:52:01 +0700

    Create zapret.ps1

---
+++ zapret.ps1
@@ -0,0 +1,139 @@
+Clear-Host
+$zapretDir = "%winDir%\Zapret"
+Write-Host "  /ZZZZZ    AAAA   PPPPP   RRRRR   EEEEE   TTTTT"
+Write-Host "      /Z   A    A  P    P  R    R  E         T"
+Write-Host "     /Z   A      A P    P  R    R  E         T"
+Write-Host "    /Z    AAAAAAAA PPPPP   RRRRR   EEEE      T"
+Write-Host "   /Z     A      A P       R   R   E         T"
+Write-Host "  /Z      A      A P       R    R  E         T"
+Write-Host " /ZZZZZ   A      A P       R     R EEEEE     T"
+Write-Host "    sevcator.github.io / github.com/bol-van"
+function Check-Admin {
+    $identity = [System.Security.Principal.WindowsIdentity]::GetCurrent()
+    $principal = New-Object System.Security.Principal.WindowsPrincipal($identity)
+    return $principal.IsInRole([System.Security.Principal.WindowsBuiltInRole]::Administrator)
+}
+if (-not (Check-Admin)) {
+    Write-Host "! Run PowerShell as administrator rights!"
+    return
+}
+$initialDirectory = Get-Location
+$osVersion = [Environment]::OSVersion.Version
+Write-Host "- Windows version: $osVersion"
+$windows10Version = New-Object System.Version(10, 0)
+if ($osVersion.Major -lt 10) {
+    Write-Host "* Enabling Test Mode for Windows less than 10" -ForegroundColor Yellow
+    $testMode = bcdedit /enum | Select-String "testsigning" -Quiet
+    
+    if (-not $testMode) {
+        bcdedit /set loadoptions DISABLE_INTEGRITY_CHECKS | Out-Null
+        bcdedit /set TESTSIGNING ON | Out-Null
+        Write-Host "* Reboot the system and run this script to countinue the installation" -ForegroundColor Yellow
+        exit 1
+    }
+}
+function Check-ProcessorArchitecture {
+    $processor = Get-WmiObject -Class Win32_Processor
+    return $processor.AddressWidth -eq 64
+}
+if (-not [Environment]::Is64BitOperatingSystem) {
+    Write-Host "* Your system is not 64-bit"
+    return
+}
+Write-Host "- Your system is 64-bit"
+Write-Host "- Terminating processes"
+$processesToKill = @("GoodbyeDPI.exe", "winws.exe", "zapret.exe")
+foreach ($process in $processesToKill) {
+   taskkill /f /im $process -ErrorAction SilentlyContinue | Out-Null
+}
+Write-Host "- Destroying services"
+$servicesToStop = @("zapret", "winws1", "goodbyedpi", "windivert", "windivert14")
+foreach ($service in $servicesToStop) {
+    $serviceStatus = Get-Service -Name $service -ErrorAction SilentlyContinue | Out-Null
+
+    if ($serviceStatus) {
+        try {
+           sc stop $service -ErrorAction SilentlyContinue | Out-Null
+        } catch {
+            Write-Host ("{0}: {1}" -f $service, $_.Exception.Message) -ForegroundColor Red
+        }
+        
+        try {
+            sc.exe delete $service -ErrorAction SilentlyContinue | Out-Null
+        } catch {
+            Write-Host ("{0}: {1}" -f $service, $_.Exception.Message) -ForegroundColor Yellow
+	          Write-Host "If you have problems, try restart your machine!" -ForegroundColor Yellow
+        }
+    } else {
+    
+    }
+}
+Write-Host "- Flushing DNS cache"
+ipconfig /flushdns -ErrorAction SilentlyContinue | Out-Null
+$exclusionPath = "$zapretDir\winws.exe"
+Write-Host "- Adding exclusion"
+if (-not (Test-Path $exclusionPath)) {
+    New-Item -Path $exclusionPath -ItemType File -ErrorAction SilentlyContinue | Out-Null
+}
+try {
+    Add-MpPreference -ExclusionPath $exclusionPath -ErrorAction SilentlyContinue
+    Start-Sleep -Seconds 5
+} catch {
+    Write-Host "- Error adding exclusion? If you have another AntiMalware software, add exclusion C:\Windows\Zapret\winws.exe, C:\Windows\Zapret\WinDivert.dll, C:\Windows\Zapret\WinDivert64.sys" -ForegroundColor Yellow
+}
+Write-Host "- Downloading files"
+$baseFiles = @(
+    "WinDivert.dll", "WinDivert64.sys", "cygwin1.dll", "winws.exe",
+    "ipset-discord.txt", "list.txt", "list-exclude.txt",
+    "tls-google.bin", "quic-google.bin", "zapret.cmd", "zapret-redirect.cmd"
+)
+$tacticsFiles = @(
+    "autohosts-bol-van.txt", "autohosts-flowseal-alt-2.txt",
+    "autohosts-flowseal-alt-3.txt", "autohosts-flowseal-alt-4.txt",
+    "autohosts-flowseal-alt-5.txt", "autohosts-flowseal-alt-6.txt",
+    "autohosts-flowseal-alt.txt", "autohosts-flowseal-mgts-2.txt",
+    "autohosts-flowseal-mgts.txt", "autohosts-flowseal.txt",
+    "hosts-bol-van.txt", "hosts-flowseal-alt-2.txt",
+    "hosts-flowseal-alt-3.txt", "hosts-flowseal-alt-4.txt",
+    "hosts-flowseal-alt-5.txt", "hosts-flowseal-alt-6.txt",
+    "hosts-flowseal-alt.txt", "hosts-flowseal-mgts-2.txt",
+    "hosts-flowseal-mgts.txt", "hosts-flowseal.txt"
+)
+$baseUrl = "https://github.com/xdxdf/zapret-ps1/raw/refs/heads/main/files"
+$tacticsUrl = "$baseUrl/tactics"
+function Download-Files($files, $baseUrl, $destination) {
+    foreach ($file in $files) {
+        try {
+            $url = "$baseUrl/$file"
+            $outFile = Join-Path $destination $file
+            Invoke-WebRequest -Uri $url -OutFile $outFile -ErrorAction Stop
+        } catch {
+            Write-Host "* Error to download $file : $($_.Exception.Message)" -ForegroundColor Red
+        }
+    }
+}
+Download-Files $baseFiles $baseUrl $zapretDir
+Download-Files $tacticsFiles $tacticsUrl $zapretDir
+Copy-Item "$zapretDir\zapret-redirect.cmd" "$system32Dir\zapret.cmd" -Force
+foreach ($file in $files) {
+    try {
+        Invoke-WebRequest -Uri $file.Url -OutFile "$zapretDir\$($file.Name)" -ErrorAction Stop | Out-Null
+    } catch {
+        Write-Host ("{0}: {1}" -f $($file.Name), $_.Exception.Message) -ForegroundColor Red
+    }
+}
+<или улучши логику выкачивания файлов tactics>
+<перемести zapret-redirect.cmd в system32 папку и переименуй в zapret.cmd>
+<прочитай файл autohosts-bol-van.txt из той папки и запиши в $ZAPRET_ARGS>
+Set-Location $zapretDir | Out-Null
+$ZAPRET_ARGS = Get-Content "$zapretDir\autohosts-bol-van.txt" -Raw
+Write-Host "- Creating service"
+try {
+    sc.exe create winws1 binPath= "`"$zapretDir\winws.exe $ZAPRET_ARGS`"" DisplayName= "zapret DPI bypass" start= auto | Out-Null
+    sc description winws1 "Bypass internet censorship via modification DPI @ by bol-van & xd.github.io" | Out-Null
+    sc.exe start winws1 | Out-Null
+} catch {
+    Write-Host ("! Failed to create or start service: {0}" -f $_.Exception.Message) -ForegroundColor Red
+}
+Write-Host "- Done!"
+
